5f86d337be4de6953728c940ecc5061014fe5f8b
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2018-10-12 23:05:29 -0300

    Adicionando o contador das contas

--- JS/app/addItem.js
+++ JS/app/addItem.js
@@ -5,7 +5,7 @@ const submitForm = e => {
     let conta = document.getElementById("nomeConta").value;
     let valor = document.getElementById("valorConta").value;
 
-    if(conta !== "" && valor !== ""){
+    if(conta !== "" && valor !== "" && !isNaN(valor)){
         const item = {conta,valorConta: Number(valor)};
         ipcRenderer.send('item:add',item);
     }
--- JS/app/index.js
+++ JS/app/index.js
@@ -1,7 +1,12 @@
 const { ipcRenderer } = require('electron');
 
+
+//crime, váriaveis globais
 let ul = document.querySelector('ul');
 let h3 = document.querySelector('.message');
+let finalVal = document.querySelector('.final-value'); 
+
+let valoresContas = [];
 
 const emptyList  = () => {
     h3.textContent = ul.children.length === 0 ? "Não há items na sua lista" : "";
@@ -17,11 +22,22 @@ const remove = e => {
 }
 
 ipcRenderer.on('item:add',(e,item) => {
+    valoresContas.push(item.valorConta);
+
     const li = templateLi(item);
     ul.innerHTML += li;
+
+    viewFinalValue(valoresContas);
     emptyList();
 })
 
+const viewFinalValue = arr =>  {
+    finalVal.textContent = `O valor final das suas contas é ${somaContas(arr)}`;
+}
+
+const somaContas = arr => {
+    return arr.reduce((valorTotal,conta) => valorTotal + conta,0).toFixed(2);
+}
 
 window.addEventListener('load', e => emptyList());
 
